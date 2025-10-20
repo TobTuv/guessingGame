0559045351197159ce2f7199dc7e3065dc9eab2e
Tobias Tuveback <ac75159@skola.goteborg.se>
2025-10-20 16:08:15 +0200

    complete getReply method

--- Guesser.java
+++ Guesser.java
@@ -1,4 +1,5 @@
 import java.io.Console;
+import java.util.Scanner;
 
 /**
  * Assignment 1 - Guessing Game
@@ -59,11 +60,17 @@ public class Guesser {
    * the player.
    */
   private String getReply() {
-    String reply = null;
     // Write code here which reads a String from the console.
     // As long as it is not a valid reply (one of "T" and "F")
     // write an error message, and read a new reply.
     // When you have gotten a valid reply, return it.
+    Scanner scanner = new Scanner(System.in);
+    String reply = scanner.nextLine();
+    while (!reply.equals("T") && !reply.equals("F")) {
+      System.out.println("ERROR! Type T or F only.");
+      reply = scanner.nextLine();
+    }
+
     return reply;
   }
 
